8054a6795e6f1d6b6903585dbf27cb4200601f99
moha2508 <30807505+moha2508@users.noreply.github.com>
2017-12-01 19:20:34 +0100

    Update find_ip_from_site.py

--- find_ip_from_site.py
+++ find_ip_from_site.py
@@ -3,10 +3,7 @@ import socket
 def app_ip():
     get_app = str(input("enter your site web :"))   
     b = socket.gethostbyname(get_app)
-    print(b)
-    if get_app == int(1):
-        ap = app_ip()
-        print(ap)
+    print("ip for site is :",b)
    
    
 app_ip()
